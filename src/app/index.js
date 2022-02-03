32e9766afab44b671985d2474a15e83b894bb1d3
James Smith <jameswsmith805@gmail.com>
2022-02-02 19:36:32 -0800

    configure babel and css loaders

---
+++ src/app/index.js
@@ -0,0 +1,9 @@
+import '../style/app.scss';
+class TestClass {
+	constructor() {
+		let msg = 'Using ES2015+ Syntax';
+		console.log(msg);
+	}
+}
+
+let test = new TestClass();
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,33 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	mode: 'development',
 	entry: __dirname + '/src/app/index.js',
+	devtool: 'inline-source-map',
 	output: {
 		path: __dirname + '/dist',
 		filename: 'bundle.js',
-		publicPath: '/',
+		path: path.resolve(__dirname, 'dist'),
 	},
 	module: {
-		rules: [],
+		rules: [
+			{
+				test: /\.js$/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+						plugins: ['@babel/plugin-proposal-object-rest-spread'],
+					},
+				},
+				exclude: [/node_modules/],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
